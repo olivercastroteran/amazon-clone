dcfbeb70bcc35040ccb8caba5e46fc214b270eed
oliver <olivercastroteran@gmail.com>
2020-09-23 19:04:29 -0400

    initial folder structure & fbConfig added

---
+++ src/config/fbConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAgDjSZfZFVFcOP_deJAfvz8SNHQ8v9yd0',
+  authDomain: 'clone-41942.firebaseapp.com',
+  databaseURL: 'https://clone-41942.firebaseio.com',
+  projectId: 'clone-41942',
+  storageBucket: 'clone-41942.appspot.com',
+  messagingSenderId: '881498478217',
+  appId: '1:881498478217:web:26f59543535472c038d858',
+};

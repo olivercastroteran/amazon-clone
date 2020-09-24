14549d4564b1dcecbf865d6b9ed8dff7fa7951ef
oliver <olivercastroteran@gmail.com>
2020-09-24 12:51:25 -0400

    subtotal component added to checkout

--- src/components/checkout/subtotal/Subtotal.js
+++ src/components/checkout/subtotal/Subtotal.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Subtotal.css';
+import CurrencyFormat from 'react-currency-format';
 
 const Subtotal = () => {
   return (
     <div className="subtotal">
-      <h1>Subtotal</h1>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal(0 items): <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        dicimalScale={2}
+        value={0}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+      />
+
+      <button>Proceed to Checkout</button>
     </div>
   );
 };

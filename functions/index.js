c0226e33328c3891ebcd1bd89490ed122e12d31c
oliver <olivercastroteran@gmail.com>
2020-09-27 23:36:40 -0400

    firebase functions init (backend) plus dependencies installed - express, cors, stripe

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const { response } = require('express');
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')(
+  'sk_test_51HW9jiHlSfG5NgkZ2sNkbdMmHUBCBVOdcPflJOeHvmEdidLmYNMnmdlrGt0oc1kqlAzEwFcxjjI4dkCnN3Uw3RGy00s5ILrn2z'
+);
+
+// API
+
+// App config
+
+// Middlewares
+
+// API routes
+
+// Listen command
